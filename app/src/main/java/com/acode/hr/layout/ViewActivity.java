997e77354a6b4dc07ebdd2d1339baeee018b5fd7
1240490684@qq.com <yangtao123456>
2019-12-10 17:38:08 +0800

    update

--- app/src/main/java/com/acode/hr/layout/ViewActivity.java
+++ app/src/main/java/com/acode/hr/layout/ViewActivity.java
@@ -3,28 +3,42 @@ package com.acode.hr.layout;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ScrollView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ViewActivity extends AppCompatActivity {
     private HrLayout hrLayout;
-    private Button btnTouchView;
+    private LinearLayout llRoot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_linear);
         hrLayout = findViewById(R.id.hrLayout);
-        btnTouchView = findViewById(R.id.btnTouchView);
-//        hrLayout.setTouchView(btnTouchView);
+        llRoot = findViewById(R.id.llRoot);
+        hrLayout.setTouchView(llRoot);
     }
 
     public void funClick(View view) {
-//        if (!hrLayout.isTop()) {
-//            hrLayout.toTop();
-//        } else {
-//            hrLayout.toBottom();
-//        }
+        if (!hrLayout.isTop()) {
+            hrLayout.toTop();
+        } else {
+            hrLayout.toBottom();
+        }
     }
+
+    public void funStart(View view) {
+        if (!hrLayout.isTop()) {
+            hrLayout.toTop();
+        }
+    }
+
+    public void funClose(View view) {
+        if (hrLayout.isTop()) {
+            hrLayout.toBottom();
+        }
+    }
+
 }
